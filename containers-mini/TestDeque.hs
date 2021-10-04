71ecedabc9d039cd2f578a762eeb73b6ab9d0a06
Penny Chase <mpchase@mac.com>
2021-10-04 08:18:53 -0400

    Added comments to track deque after operations

--- containers-mini/TestDeque.hs
+++ containers-mini/TestDeque.hs
@@ -1,15 +1,15 @@
 import Data.Deque
 
 main = do
-    let st = push_front 15 $ push_front 10 $ push_front 5 $ push_front 0 empty
-        -- st == 15, 10, 5, 0 
-        st' = pop_front $ pop_front st -- 5, 0 
-        st'' = push_front 100 st' -- 100, 5, 0 
-        st''' = push_back 50 st' -- 5, 0, 50
-        shouldBeTrue = [ front st' == Just 5,
-                         front st'' == Just 100,
-                         back st''' == Just 50,
-                         isEmpty $ pop_front $ pop_front st',
-                         isEmpty $ pop_back $ pop_back st'
+    let d = push_front 15 $ push_front 10 $ push_front 5 $ push_front 0 empty
+        -- d == 15, 10, 5, 0 <-- back
+        d' = pop_front $ pop_front d -- 5, 0 <-- back
+        d'' = push_front 100 d' -- 100, 5, 0 <-- back
+        d''' = push_back 50 $ push_back 60 $ d' -- 5, 0, 50
+        shouldBeTrue = [ front d' == Just 5,
+                         front d'' == Just 100,
+                         back d''' == Just 50,
+                         isEmpty $ pop_front $ pop_front d',
+                         isEmpty $ pop_back $ pop_back d'
                         ]
     print $ and shouldBeTrue
